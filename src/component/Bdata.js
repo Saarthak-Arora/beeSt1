5743870f5d3959a529e6d4f74bcaf02672194b40
Saarthak-Arora <saarthak1696.be20@chitkara.edu.in>
2022-11-30 13:56:33 +0530

    basic structure

---
+++ src/component/Bdata.js
@@ -0,0 +1,28 @@
+const d = [
+    
+    {   id : 1,
+        title  : "What is Javascript",
+        content : "A high-level definition JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area.",
+        author  : "Sita Ram"
+
+    },
+    {   id : 2,
+        title  : "What is Javascript",
+        content : "A high-level definition JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area.",
+        author  : "Sita Ram"
+
+    },
+    {   id : 3,
+        title  : "What is Javascript",
+        content : "javascript is javascript",
+        author  : "Sita Ram"
+
+    },
+    {   id : 4,
+        title  : "What is Javascript",
+        content : "javascript is javascript",
+        author  : "Sita Ram"
+
+    },
+]
+export default d;
---
+++ src/component/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="i-sk">
+        <div className="text">
+          <h1>
+            Kickstart your learning with us !! Here are some knowlegeable blogs
+            on most loved language of todays era of web -- javascript.
+            <br />
+            You are always welcome to contribute your articles and spread
+            knowledge all over the world
+          </h1>
+        </div>
+        <div className="pic">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
